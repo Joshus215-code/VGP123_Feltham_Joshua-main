68fdfc3ce1b8121d16aab9d4df311cbd7bd5f09d
Joshus215-code <77842162+Joshus215-code@users.noreply.github.com>
2021-02-26 12:37:53 -0500

    Assignment 1.2

--- Assets/scripts/player/PlayerCollision.cs
+++ Assets/scripts/player/PlayerCollision.cs
@@ -27,7 +27,7 @@ public class PlayerCollision : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         
-        if(collision.gameObject.tag == "Enemy")
+        if(collision.gameObject.tag == "enemy")
         {
             if (!pm.isGrounded)
             {
